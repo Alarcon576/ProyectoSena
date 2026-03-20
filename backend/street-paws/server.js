310a071d27b8e45e39b0acce10803711a4bbf6cb
Cristian Alarcon <cristianalejanll2005@gmail.com>
2026-03-20 09:20:13 -0500

    005 (Api) agregada conexion para poder trabajar con React

--- backend/street-paws/server.js
+++ backend/street-paws/server.js
@@ -1,9 +1,14 @@
 import "dotenv/config"; 
+import { cors } from "cors";
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}))
 
 const app = express();
 
@@ -12,4 +17,4 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => console.log("Servidor corriendo 🔥"));
+app.listen(3000, () => console.log("Servidor corriendo "));

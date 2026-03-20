28ea787addcaf5b4d27efa48dbe83c46583bb8e7
Cristian Alarcon <cristianalejanll2005@gmail.com>
2026-03-20 10:23:58 -0500

    006(Autenticacion):Agrega para consumo del api local

--- backend/street-paws/server.js
+++ backend/street-paws/server.js
@@ -1,20 +1,23 @@
-import "dotenv/config"; 
-import { cors } from "cors";
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+const app = express(); // 🔹 Primero declaramos app
+
+// Middlewares
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true
-}))
-
-const app = express();
-
+}));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
+// Rutas
+app.use("/api/auth", authRoutes);   
 
-app.listen(3000, () => console.log("Servidor corriendo "));
+// Servidor
+app.listen(3000, () => console.log("Servidor corriendo"));
